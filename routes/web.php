40bc1ca65c5fe2fe289ee744e6eee75942f0ea01
unknown <nafisantuyy@gmail.com>
2024-10-16 16:20:03 +0700

    add index dan login page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/index', function () {
+    return view('index');  //merujuk ke file welcome.blade.php di folder resource/views
+});
+
+Route::get('/welcome', function () {
+    return view('welcome');  //merujuk ke file welcome.blade.php di folder resource/views
+});
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');  //merujuk ke file welcome.blade.php di folder resource/views
 });
